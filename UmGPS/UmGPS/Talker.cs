cb974e19693ff2f61eb547d084d8d10a35d1537b
felipeprestess <felipeadriano_pc@hotmail.com>
2016-06-21 11:28:52 -0300

    Formulário de monstro

---
+++ UmGPS/UmGPS/Talker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UmGPS
+{
+    class Talker
+    {
+        public static int BlahBlahBlah(string thingToSay, int numberOfTimes)
+        {
+            string finalString = "";
+            for (int count = 1; count <= numberOfTimes; count++)
+            {
+                finalString = finalString + thingToSay + "\n";
+            }
+            MessageBox.Show(finalString);
+
+            return finalString.Length;
+        }
+    }
+}
