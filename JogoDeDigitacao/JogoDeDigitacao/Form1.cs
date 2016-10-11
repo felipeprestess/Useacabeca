28fe9e8d127e2ae44f0e0508ad62ce1af5c85500
felipeprestess <felipeadriano_pc@hotmail.com>
2016-10-11 18:40:54 -0300

    Inserido jogo das letras

---
+++ JogoDeDigitacao/JogoDeDigitacao/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDeDigitacao
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        Stats stats;
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+            stats = new Stats();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 90));//Trocar Random por random
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game over");
+                timer1.Stop();
+            }
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 20;
+                else if (timer1.Interval > 250)
+                    timer1.Interval -= 14;
+                else if (timer1.Interval > 100)
+                    timer1.Interval -= 8;
+
+                difficultyProgressBar.Value = 800 - timer1.Interval;
+ 
+
+                stats.Update(true);
+
+            }
+            else
+            {
+                stats.Update(false);
+            }
+            accuracyLabel.Text = "Accuracy: " + stats.Accuracy + "%";
+            correctLabel.Text = "Correct: " + stats.Correct;
+            missedLabel.Text = "Missed: " + stats.Missed;
+            totalLabel.Text = "Total: " + stats.Total;
+        }
+    }
+}
