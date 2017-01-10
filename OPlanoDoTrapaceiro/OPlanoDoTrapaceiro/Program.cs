4ec590262d91329c1f34c3352c76fbc3ea8dd6b6
felipeprestess <felipeadriano_pc@hotmail.com>
2017-01-10 16:14:02 -0200

    Exercício do plano do trapaceiro

---
+++ OPlanoDoTrapaceiro/OPlanoDoTrapaceiro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OPlanoDoTrapaceiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\secret_plan.txt");
+            sw.WriteLine("How I'll defeat captain Amazing");
+            sw.WriteLine("Another genius secret lan by The Swidler");
+            sw.Write("I'll create an army of clones and ");
+            sw.WriteLine("unleash them upon the citizens of Objectville.");
+            string location = "the mall";
+            for (int number = 0; number <= 6; number++)
+            {
+                sw.WriteLine("Clone #{0} attacks {1}", number, location);
+                if(location == "the mall") { location = "downtown"; }
+                else { location = "the mall"; }
+            }
+            sw.Close();
+        }
+    }
+}
