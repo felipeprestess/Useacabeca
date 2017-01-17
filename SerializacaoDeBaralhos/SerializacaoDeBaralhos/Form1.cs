11376b3fea533f9b86c6827e059382e205181788
felipeprestess <felipeadriano_pc@hotmail.com>
2017-01-17 12:30:37 -0200

    Exercício de serialização de baralhos feito

---
+++ SerializacaoDeBaralhos/SerializacaoDeBaralhos/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializacaoDeBaralhos
+{
+    public partial class Form1 : Form
+    {
+        private Game game;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("Please enter your name", "Can't start game yet");
+                return;
+            }
+            game = new Game(txtName.Text, new List<string> { "Joe", "Bob" }, txtProgress);
+            btnStart.Enabled = false;
+            txtName.Enabled = false;
+            btnAsk.Enabled = true;
+            UpdateForm();
+        }
+
+        private void UpdateForm()
+        {
+            listHand.Items.Clear();
+            foreach (string cardName in game.GetPlayerCardNames())
+                listHand.Items.Add(cardName);
+            txtBooks.Text = game.DescribeBooks();
+            txtProgress.Text += game.DescribePlayerHands();
+            txtProgress.SelectionStart = txtProgress.Text.Length;
+            txtProgress.ScrollToCaret();
+        }
+
+        private void btnAsk_Click(object sender, EventArgs e)
+        {
+            txtProgress.Text = "";
+            if (listHand.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please select a card");
+                return;
+            }
+            if (game.PlayOneRound(listHand.SelectedIndex))
+            {
+                txtProgress.Text += "The winner is ..." + game.GetWinnerName();
+                txtBooks.Text = game.DescribeBooks();
+                btnAsk.Enabled = false;
+            }
+            else
+                UpdateForm();
+        }
+
+        private Deck RandomDeck(int number)
+        {
+            Deck myDeck = new Deck(new Card[] { });
+            for (int i = 0; i < number; i++)
+            {
+                myDeck.Add(new Card(
+                    (Suits)random.Next(4),
+                    (Values)random.Next(1, 14)));
+            }
+            return myDeck; 
+        }
+
+        private void DealCards(Deck deckToDeal, string title)
+        {
+            Console.WriteLine(title);
+            while (deckToDeal.Count > 0)
+            {
+                Card nextCard = deckToDeal.Deal(0);
+                Console.WriteLine(nextCard.Name);
+            }
+            Console.WriteLine("----------------------");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Deck deckToWrite = RandomDeck(5);
+            using (Stream output = File.Create("Deck1.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(output, deckToWrite);
+            }
+            DealCards(deckToWrite, "What I just wrote to the file");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (Stream input = File.OpenRead("Deck1.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                Deck deckFromFile = (Deck)bf.Deserialize(input);
+                DealCards(deckFromFile, "What I rad from the file");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (Stream output = File.Create("Deck2.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+
+                for (int i = 1; i <= 5; i++)
+                {
+                    Deck deckToWrite = RandomDeck(random.Next(1, 10));
+                    bf.Serialize(output, deckToWrite);
+                    DealCards(deckToWrite, "Deck #" + i + " written");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (Stream input = File.OpenRead("Deck2.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                for (int i = 1; i <= 5; i++)
+                {
+                    Deck deckToRead = (Deck)bf.Deserialize(input);
+                    DealCards(deckToRead, "Deck #" + i + " read");
+                }
+            }
+        }
+    }
+}
---
+++ SerializacaoDeBaralhos/SerializacaoDeBaralhos/Form1.designer.cs
@@ -0,0 +1,223 @@
+﻿namespace SerializacaoDeBaralhos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listHand = new System.Windows.Forms.ListBox();
+            this.btnAsk = new System.Windows.Forms.Button();
+            this.lblYourHand = new System.Windows.Forms.Label();
+            this.lblYourName = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtProgress = new System.Windows.Forms.TextBox();
+            this.txtBooks = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listHand
+            // 
+            this.listHand.FormattingEnabled = true;
+            this.listHand.Location = new System.Drawing.Point(497, 34);
+            this.listHand.Name = "listHand";
+            this.listHand.Size = new System.Drawing.Size(152, 329);
+            this.listHand.TabIndex = 0;
+            // 
+            // btnAsk
+            // 
+            this.btnAsk.Enabled = false;
+            this.btnAsk.Location = new System.Drawing.Point(497, 369);
+            this.btnAsk.Name = "btnAsk";
+            this.btnAsk.Size = new System.Drawing.Size(152, 29);
+            this.btnAsk.TabIndex = 1;
+            this.btnAsk.Text = "Ask for a card";
+            this.btnAsk.UseVisualStyleBackColor = true;
+            this.btnAsk.Click += new System.EventHandler(this.btnAsk_Click);
+            // 
+            // lblYourHand
+            // 
+            this.lblYourHand.AutoSize = true;
+            this.lblYourHand.Location = new System.Drawing.Point(497, 16);
+            this.lblYourHand.Name = "lblYourHand";
+            this.lblYourHand.Size = new System.Drawing.Size(56, 13);
+            this.lblYourHand.TabIndex = 2;
+            this.lblYourHand.Text = "Your hand";
+            // 
+            // lblYourName
+            // 
+            this.lblYourName.AutoSize = true;
+            this.lblYourName.Location = new System.Drawing.Point(12, 16);
+            this.lblYourName.Name = "lblYourName";
+            this.lblYourName.Size = new System.Drawing.Size(58, 13);
+            this.lblYourName.TabIndex = 3;
+            this.lblYourName.Text = "Your name";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(237, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(242, 22);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start the game!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(15, 34);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(216, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtProgress
+            // 
+            this.txtProgress.Location = new System.Drawing.Point(12, 82);
+            this.txtProgress.Multiline = true;
+            this.txtProgress.Name = "txtProgress";
+            this.txtProgress.ReadOnly = true;
+            this.txtProgress.Size = new System.Drawing.Size(467, 197);
+            this.txtProgress.TabIndex = 6;
+            // 
+            // txtBooks
+            // 
+            this.txtBooks.Location = new System.Drawing.Point(12, 298);
+            this.txtBooks.Multiline = true;
+            this.txtBooks.Name = "txtBooks";
+            this.txtBooks.ReadOnly = true;
+            this.txtBooks.Size = new System.Drawing.Size(467, 100);
+            this.txtBooks.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Game progress";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Books";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "save random";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(337, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "load random";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 455);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBooks);
+            this.Controls.Add(this.txtProgress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblYourName);
+            this.Controls.Add(this.lblYourHand);
+            this.Controls.Add(this.btnAsk);
+            this.Controls.Add(this.listHand);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Go Fish!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listHand;
+        private System.Windows.Forms.Button btnAsk;
+        private System.Windows.Forms.Label lblYourHand;
+        private System.Windows.Forms.Label lblYourName;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtProgress;
+        private System.Windows.Forms.TextBox txtBooks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
