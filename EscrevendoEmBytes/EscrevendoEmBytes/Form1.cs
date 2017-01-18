9ca03cc5023e22e05f5b5a9a98827c86617feb7d
felipeprestess <felipeadriano_pc@hotmail.com>
2017-01-18 19:42:34 -0200

    Exercício de escrita e leitura em bytes, hexadecimal e hebraico

---
+++ EscrevendoEmBytes/EscrevendoEmBytes/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace EscrevendoEmBytes
+{
+    public partial class Form1 : Form
+    {
+        private string filePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Escrevendo em bytes
+            File.WriteAllText("Eureka.txt", "Eureka!");
+            byte[] eurekaBytes = File.ReadAllBytes("Eureka.txt");
+
+            foreach (byte b in eurekaBytes)
+                Console.Write("{0} ", b);
+            Console.WriteLine();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Escrevendo em hexadecimal
+            File.WriteAllText("Eureka.txt", "Eureka!");
+            byte[] eurekaBytes = File.ReadAllBytes("Eureka.txt");
+
+            foreach (byte b in eurekaBytes)
+                Console.Write("{0:x2} ", b);
+            Console.WriteLine();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("Eureka.txt", "שלום", Encoding.Unicode);
+            byte[] eurekaBytes = File.ReadAllBytes("Eureka.txt");
+
+            foreach (byte b in eurekaBytes)
+                Console.Write("{0:x2} ", b);
+            Console.WriteLine();
+        }
+    }
+}
