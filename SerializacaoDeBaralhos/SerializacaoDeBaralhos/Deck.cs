125199f6be987ed424f48883dcf72d97ac38ab41
felipeprestess <felipeadriano_pc@hotmail.com>
2017-02-14 22:44:03 -0200

    Implementando parte do código do construtor sobrecarregado Deck

--- SerializacaoDeBaralhos/SerializacaoDeBaralhos/Deck.cs
+++ SerializacaoDeBaralhos/SerializacaoDeBaralhos/Deck.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SerializacaoDeBaralhos
 {
@@ -23,6 +24,47 @@ namespace SerializacaoDeBaralhos
         public Deck(IEnumerable<Card> initialCards)
         {
             cards = new List<Card>(initialCards);
+        }
+
+        public Deck(string filename)
+        {
+            cards = new List<Card>();
+            StreamReader reader = new StreamReader(filename);
+            while (!reader.EndOfStream)
+            {
+                bool invalidCard = false;
+                string nextCard = reader.ReadLine();
+                string[] cardParts = nextCard.Split(new char[] { ' ' });
+                Values value = Values.Ace;
+                switch (cardParts[0])
+                {
+                    case "Ace": value = Values.Ace;break;
+                    case "Two": value = Values.Two; break;
+                    case "Three": value = Values.Three; break;
+                    case "Four": value = Values.Four; break;
+                    case "Five": value = Values.Five; break;
+                    case "Six": value = Values.Six; break;
+                    case "Seven": value = Values.Seven; break;
+                    case "Eight": value = Values.Eight; break;
+                    case "Nine": value = Values.Ace; break;
+                    case "Ten": value = Values.Ten; break;
+                    case "Jack": value = Values.Jack; break;
+                    case "Queen": value = Values.Queen; break;
+                    case "King": value = Values.King;break;
+                    default: invalidCard = false; break;
+                }
+                Suits suit = Suits.Clubs;
+                switch (cardParts[2])
+                {
+                    case "Spades": suit = Suits.Spades; break;
+                    case "Clubs": suit = Suits.Clubs; break;
+                    case "Hearts": suit = Suits.Hearts; break;
+                    case "Diamonds": suit = Suits.Diamonds; break;
+                    default: invalidCard = false; break;
+                }
+                if (!invalidCard)
+                    cards.Add(new Card(suit, value));
+            }
         }
 
         public int Count { get { return cards.Count; } }
