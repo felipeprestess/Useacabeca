ae3c122966339562646bfc92671027ac2f7ca65d
felipeprestess <felipeadriano_pc@hotmail.com>
2017-07-12 18:03:10 -0300

    Adicionando commits

---
+++ AprendendoEventos/AprendendoEventos/Bat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoEventos
+{
+    class Bat
+    {
+        private BatCallback hitBallCallback;
+
+        public Bat(BatCallback callbackDelegate)
+        {
+            this.hitBallCallback = new BatCallback(callbackDelegate);
+        }
+
+        public void HitTheBall(BallEventArgs e)
+        {
+            if (hitBallCallback != null)
+            {
+                hitBallCallback(e);
+            }
+        }
+    }
+}
